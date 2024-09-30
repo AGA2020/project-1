95a1be99766d133c5832f5f9aae48816e3faf544
AGA2020 <hasan9u@gmail.com>
2024-09-30 12:25:15 +0300

    peacetime training günlük xp veriyor

--- common/defines/elwolfmod_defines.lua
+++ common/defines/elwolfmod_defines.lua
@@ -45,6 +45,7 @@ NDefines.NProduction.BASE_LICENSE_IC_COST = 0						    -- Base IC cost for lende
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0 	--Base cost to unlock a regiment slot,
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0	--Base cost to change a regiment column.
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0 	--Base cost to unlock a support slot
+NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0.000000001
 -- Free Designs
 NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0				    -- XP cost for adding a new equipment module in an empty slot when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
